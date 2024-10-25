6febb970663a261c782d726aea295a1070d81b5f
aliaabdalrhman <aliaabdalrhman2001@gmail.com>
2024-10-25 13:15:52 +0300

    Handle Error

--- Src/InitApp.js
+++ Src/InitApp.js
@@ -6,8 +6,14 @@ const initApp = (app, express) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(cors());
-    app.get('*', (req, res) => {
-        return res.status(404).json({ message: "page not found" });
-    })
+    app.get('*', (req, res, next) => {
+        return next(new AppError("page not found", 404));
+    });
+    app.use((success, req, res, next) => {
+        return res.status(success.statusCode).json({ message: success.message, ...success.additionalData });
+    });
+    app.use((err, req, res, next) => {
+        return res.status(err.statusCode).json({ message: err.message });
+    });
 }
 export default initApp;
