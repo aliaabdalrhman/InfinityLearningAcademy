08419bf631c1a4067c1ba0b4580f79d080d27aa4
aliaabdalrhman <aliaabdalrhman2001@gmail.com>
2024-10-25 13:31:08 +0300

    Create User Model

---
+++ DB/Models/User.model.js
@@ -0,0 +1,59 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String
+    },
+    phone: {
+        type: Number,
+    },
+    address: {
+        type: String
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
+    confirmEmail: {
+        type: Boolean,
+        default: false
+    },
+    gender: {
+        type: String,
+    }
+    ,
+    status: {
+        type: String,
+        enum: ['Active', 'InActive'],
+        default: 'Active'
+
+    },
+    role: {
+        type: String,
+        enum: ['SuperAdmin','Admin', 'Instructor','Student'],
+        default: 'User'
+    },
+    sendCode: {
+        type: String,
+        default: null
+    }
+},
+    {
+        timestamps: true
+    });
+
+const userModel = model('User', userSchema);
+
+export default userModel;
